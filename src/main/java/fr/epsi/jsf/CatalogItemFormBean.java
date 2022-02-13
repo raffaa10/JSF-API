4a7592d82cb41873e0ed022e467881e17c879276
Rafayel Azizyan <rafayel.azizyan@gmail.com>
2022-02-13 02:16:09 +0100

    Advanced_backing_beans

--- src/main/java/fr/epsi/jsf/CatalogItemFormBean.java
+++ src/main/java/fr/epsi/jsf/CatalogItemFormBean.java
@@ -1,41 +1,51 @@
 package fr.epsi.jsf;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
-@RequestScoped
+@SessionScoped
 @Named
-public class CatalogItemFormBean {
+public class CatalogItemFormBean implements Serializable {
 	
-	private String name;
-	private String manufacturer;
-	private String sku;
+	private CatalogItem catalogItem = new CatalogItem();
 	
+	private List<CatalogItem> items = new ArrayList<>();
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getManufacturer() {
-		return manufacturer;
-	}
-
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
-
-	public String getSku() {
-		return sku;
-	}
-
-	public void setSku(String sku) {
-		this.sku = sku;
-	}
 	
+	public String addItem() {
+		
+		long itemId = this.items.size() + 1;
+		this.items.add(new CatalogItem(itemId, this.catalogItem.getName(),
+				this.catalogItem.getManufacturer(),
+				this.catalogItem.getDescription(),
+				this.catalogItem.getAvailableDate()
+				));
+		this.items.stream().forEach(item -> {
+			System.out.println(item.toString());
+		});
+		return "list?faces-redirect=true";
+	}
+
+	public CatalogItem getCatalogItem() {
+		return catalogItem;
+	}
+
+	public void setCatalogItem(CatalogItem catalogItem) {
+		this.catalogItem = catalogItem;
+	}
+
+	public List<CatalogItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<CatalogItem> items) {
+		this.items = items;
+	}
 	
 	
 
